2c9cfb542b8de242686d487dea060df4f049f375
Rishab <rishabagrawal@hotmail.com>
2019-05-21 15:14:00 -0700

    add footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -23,16 +23,18 @@
 						<span><i class="fab fa-google-plus-square"></i></span></p>
 					</div>
                 </div>	
+
 				<div class="footer-block-item">
 					<div class="business-hours">
 						<h3>Business Hours</h3>
-						<p><span class="day of week">Monday-Friday:</span> 9am to 5pm</p>
+						<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
 						<p><span class="day of week">Saturday:</span> 10am to 2pm </p>
 						<p><span class="day of week">Sunday:</span> Closed </p> 
 					</div>
-				</div>	
-				<div class="footer-block-item">
-				<div class="text-logo"><a href="#"><img src="wp-content/themes/redstarter/assets/images/logos/inhabitent-logo-text-dark.svg" alt="inhabitent logo"></a>
+				</div>
+
+				<div class="footer-block-item logo">
+				<div class="text-logo"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-text-dark.svg" alt="inhabitent logo"></a>
                </div>
 				</div>
 				
