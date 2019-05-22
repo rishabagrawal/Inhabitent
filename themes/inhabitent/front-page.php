fc3e730a12e703ec87e763666a9c03d59a03b068
Rishab <rishabagrawal@hotmail.com>
2019-05-22 14:39:15 -0700

    add journal section in front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -9,14 +9,41 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		  <section class="home-hero">
+		  <section class="home-hero"> </section>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<section class= "fp-journal">
+			<h2>Inhabitent Journal</h2>
+				
+				<?php
+	$args = array( 'post_type' => 'post', 'order' => 'ASC', 'posts_per_page' => 3 );
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+	$journal_posts = get_posts( $args ); // returns an array of posts
+	?>
+	<div class="journal-entry-entry">
+	<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+	
+	<div class="journal entry">
+		<?php if(has_post_thumbnail()):?>
+		<div class="thumbnail-wrapper">
+			<!-- <img src="<?php // echo get_the_post_thumbnail(); ?>"> -->
+			<?php the_post_thumbnail('large'); ?>
+	</div>
+	<?php endif; ?>
 
-			<?php endwhile; // End of the loop. ?>
+	<div class="fp-post-meta">
+		<?php echo get_the_date(); ?> /
+		<?php echo get_comments_number(); ?> Comments 
+		</div>
 
+	<a href="<php echo get_thepermalink(); ?>"><h2><?php the_title(); ?></h2></a>
+	<a class="read-more-btn" href="<?php echo get_the_permalink(); ?>">Read Entry</a>
+	</div>	
+
+		 
+	
+	<?php endforeach; wp_reset_postdata(); ?>
+	</div>
+</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
